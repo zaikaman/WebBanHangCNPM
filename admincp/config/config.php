17b740a20c0aca39c09592f10628907d480ff028
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-24 23:56:50 +0700

    Changed config

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,9 +1,9 @@
- <?php
-$mysqli = new mysqli("localhost","root","","web_quanao");
+<?php
+$mysqli = new mysqli("ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ot9yhdbg6jaf15t0", "u5loufopei3laxcr", "dj61lxep55zuk7bd", 3306);
 
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
-?> 
+?>
