a2af7a7c5c716f71a6558b4589df30aa7e38f8f6
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-10-26 09:42:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/css/style-v3.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <title>7TCC</title>
     <!-- LINK FONT STYLE -->
     <link
