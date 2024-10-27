58c4505d9d779e3b3db7daf592c11857bc35dcb8
capijim <153265171+capijim@users.noreply.github.com>
2024-10-27 17:22:16 +0700

    Update config_vnpay.php

--- pages/main/config_vnpay.php
+++ pages/main/config_vnpay.php
@@ -9,7 +9,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 $vnp_TmnCode = "IZK6416P"; //Mã định danh merchant kết nối (Terminal Id)
 $vnp_HashSecret = "2GR2VBFLLLTXIRQMKKMG2OEPAR72VJ8N"; //Secret key
 $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-$vnp_Returnurl = "http://7tcc.atwebpages.com/index.php?quanly=camon";
+$vnp_Returnurl = "https://web7tcc-a9aaa5d624b4.herokuapp.com/index.php?quanly=camon";
 $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
 $apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 //Config input format
