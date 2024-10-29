c6727aff85d817dc8c5e18a272a7de6c7c32e0e5
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-29 12:24:17 +0700

    Update lienhe.php

--- pages/main/lienhe.php
+++ pages/main/lienhe.php
@@ -1,14 +1,11 @@
 <div class="main_content">
-    <p><strong>Liên hệ</strong> </p>
+    <h2><strong>Liên hệ</strong></h2>
     <?php
     $sql_lh = "SELECT * FROM tbl_lienhe WHERE id=1";
     $query_lh = mysqli_query($mysqli, $sql_lh);
-    ?>
-    <?php
+
     while ($row = mysqli_fetch_array($query_lh)) {
-    ?>
-        <p> <?php echo $row['thongtinlienhe'] ?> </p>
-    <?php
+        echo htmlspecialchars_decode($row['thongtinlienhe']);
     }
     ?>
 </div>
