4b4bad6f467a5a6a914a8ad429ba9e4692b3203d
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-30 17:11:34 +0700

    Add composer.lock for dependency management

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '029e466e1f1be6b5a819246d961ee8655607600e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '029e466e1f1be6b5a819246d961ee8655607600e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
