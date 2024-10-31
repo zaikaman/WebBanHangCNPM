1f269d7f1298759cebaa74216a1416b22790ec1f
capijim <153265171+capijim@users.noreply.github.com>
2024-10-31 17:57:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-document.getElementById("tang").addEventListener("click", function(event) {
-    event.preventDefault();
-    let quantityInput = document.getElementById("soluong_input");
-    let currentValue = parseInt(quantityInput.value);
-    quantityInput.value = currentValue + 1;
-});
+// document.getElementById("tang").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     let quantityInput = document.getElementById("soluong_input");
+//     let currentValue = parseInt(quantityInput.value);
+//     quantityInput.value = currentValue + 1;
+// });
 
-document.getElementById("giam").addEventListener("click", function(event) {
-    event.preventDefault();
-    let quantityInput = document.getElementById("soluong_input");
-    let currentValue = parseInt(quantityInput.value);
+// document.getElementById("giam").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     let quantityInput = document.getElementById("soluong_input");
+//     let currentValue = parseInt(quantityInput.value);
     
-    if (currentValue > 1) { // Đảm bảo giá trị không xuống dưới 1
-        quantityInput.value = currentValue - 1;
-    }
-});
+//     if (currentValue > 1) { // Đảm bảo giá trị không xuống dưới 1
+//         quantityInput.value = currentValue - 1;
+//     }
+// });
 
 // Show the first tab and hide the rest
 $('#tabs-nav li:first-child').addClass('active');
