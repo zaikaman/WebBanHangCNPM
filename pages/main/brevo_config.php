a687d2c8ed1ad48de4b925d9a8cdb7f58c9ba355
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-04 18:38:12 +0700

    Update brevo_config.php

--- pages/main/brevo_config.php
+++ pages/main/brevo_config.php
@@ -4,7 +4,7 @@
 // Fetch the API key parts
 $apiKey1 = 'xkeysib';
 $apiKey2 = 'ab004c6e42d57aff3d285ffb5c9775f8d6bb2070b28cd22bfd6efe634dea1e27';
-$apiKey3 = '0d5J2SMB9B4hsFlO';
+$apiKey3 = 'E5DJhCPyUnxH69tM';
 
 // Combine the parts to form the full API key
 $apiKey = $apiKey1 . '-' . $apiKey2 . '-' . $apiKey3;
