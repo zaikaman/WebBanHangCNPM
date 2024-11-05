06fffbd705860104450eafd897f63a5017625208
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-05 14:21:52 +0700

    Update xuly.php

--- admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
@@ -1,9 +1,8 @@
 <?php
-include('..//..//config/config.php');
+include('../../config/config.php');
 
-if (isset($_POST['xoaTaiKhoan'])) {
-    $id = $_GET['id'];
-    $sql_xoa = "DELETE FROM tbl_dangky WHERE id_dangky='" . $id . "'";
-    mysqli_query($mysqli, $sql_xoa);
-    header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
-}
+$id = $_GET['id'];
+$sql_sua = "UPDATE tbl_dangky SET id_dangky = 2 WHERE id_dangky = '" . $id . "'";
+mysqli_query($mysqli, $sql_sua);
+header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
+?>
