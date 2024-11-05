3e6cc8f50d279e668e4e0d50cb031e8cf1db2226
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-05 14:27:17 +0700

    Update xuly.php

--- admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
@@ -2,7 +2,7 @@
 include('../../config/config.php');
 
 $id = $_GET['id'];
-$sql_sua = "UPDATE tbl_dangky SET id_dangky = 2 WHERE id_dangky = '" . $id . "'";
+$sql_sua = "UPDATE tbl_dangky SET id_taikhoan = 2 WHERE id_dangky = '" . $id . "'";
 mysqli_query($mysqli, $sql_sua);
 header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
 ?>
