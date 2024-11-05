214a5456ac6d0e7b2c4f689d99df252f5ea0df88
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-05 13:27:19 +0700

    Update lietke.php

--- admincp/modules/quanLyTaiKhoanKhachHang/lietke.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/lietke.php
@@ -1,36 +1,28 @@
 <?php
 	include("config/config.php");
-    $sql_lietke_khachhang = "SELECT * FROM tbl_dangky ORDER BY id_dangky DESC";
+    $sql_lietke_khachhang = "SELECT * FROM tbl_dangky ORDER BY id_dangky";
     $lietke_khachhang = mysqli_query($mysqli, $sql_lietke_khachhang);
 ?>
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- <style>
-    .col-id { width: 150px; }
-    .col-name { width: 250px; }
-    .col-address { width: 400px; }
-</style> -->
 
 <div class="container mt-5">
-    <h3 class="text-center">Liệt kê khách hàng</h3>
-    <table class="table table-bordered">
-        <thead class="table-dark" style="text-align:center">
+    <h3 class="text-center mb-4">Liệt kê khách hàng</h3>
+    <table class="table table-bordered table-hover text-center">
+        <thead class="table-dark">
             <tr>
                 <td >ID Khách hàng</td>
                 <td >Tên khách hàng</td>
-                <td style="width:400px;">Địa chỉ</td>
+                <td >Địa chỉ</td>
                 <td >Số điện thoại</td>
                 <td >Email</td>
                 <td >Trạng thái</td>             
                 <td >Quản Lý</td>
             </tr>
         </thead>
-
         <tbody>
             <?php
-            $i = 0;
             while ($row = mysqli_fetch_array($lietke_khachhang)) {
-                $i++;
             ?>
             <tr>
                 <td><?php echo $row['id_dangky']; ?></td>
