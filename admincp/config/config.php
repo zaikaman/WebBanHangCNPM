cbe93036f32ec053d80aeaa363c354d1587a7535
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-07 13:55:30 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ot9yhdbg6jaf15t0", "u5loufopei3laxcr", "dj61lxep55zuk7bd", 3306);
+$mysqli = new mysqli("o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "qpd8l97yha3kmm0y", "on8kor6fvgjukajv", "vdgpk50m2yvb0v8o", 3306);
 
 // Check connection
 if ($mysqli -> connect_errno) {
