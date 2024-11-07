6cabd034afa60fd03dd63e2c687c0f49f1eb6190
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-07 13:05:28 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,15 +1,13 @@
 <?php
-// Database connection variables
-$host = "ep-dry-tree-a4025s4m.us-east-1.aws.neon.tech";
-$dbname = "verceldb";
-$user = "default";
-$password = "BOqb3QaDuNC7";
-$port = "5432";
+// Database connection variables from the provided URL
+$dsn = "pgsql:host=cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com;port=5432;dbname=dav3lqsjounjo8";
+$user = "u56e312a1lr0ld";
+$password = "p72eafe3c2c7e3af2333278b52bd1311ed515810977a45c77f9ac715496cd0de2";
 $sslmode = "require";
 
 try {
     // Set up a new PDO connection for PostgreSQL
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode", $user, $password);
+    $pdo = new PDO("$dsn;sslmode=$sslmode", $user, $password);
 
     // Set error mode to exceptions for debugging
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
