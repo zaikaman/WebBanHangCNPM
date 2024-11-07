92ce5778d60e066f22adc6ec389ea145b6615933
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-07 13:25:42 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,19 +1,15 @@
 <?php
-// Database connection variables from the provided URL
-$dsn = "pgsql:host=cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com;port=5432;dbname=dav3lqsjounjo8";
-$user = "u56e312a1lr0ld";
-$password = "p72eafe3c2c7e3af2333278b52bd1311ed515810977a45c77f9ac715496cd0de2";
-$sslmode = "require";
+// Database connection variables
+$dsn = "mysql:host=o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=vdgpk50m2yvb0v8o";
+$user = "qpd8l97yha3kmm0y";
+$password = "on8kor6fvgjukajv";
 
 try {
-    // Set up a new PDO connection for PostgreSQL
-    $pdo = new PDO("$dsn;sslmode=$sslmode", $user, $password);
-
-    // Set error mode to exceptions for debugging
+    // Set up a new PDO connection for MySQL
+    $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+
 } catch (PDOException $e) {
-    echo "Failed to connect to PostgreSQL: " . $e->getMessage();
     exit();
 }
 ?>
