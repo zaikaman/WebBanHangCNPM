6fe483e8b025c357232d2d67514d6a74963ae04c
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-08 11:53:53 +0700

    Update xuly.php

--- admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
@@ -2,18 +2,16 @@
 include('../../config/config.php');
 
 $id = $_GET['id'];
+if (isset($id) && is_numeric($id)) {
+    $sql_xoa = "DELETE FROM tbl_dangky WHERE id_dangky = '$id'";
 
-// if (isset($id) && is_numeric($id)) {
-    $sql_sua = "UPDATE tbl_taikhoan SET trang_thai = 2 WHERE id_dangky = '$id'";
-
-    // if (mysqli_query($mysqli, $sql_sua)) {
-        mysqli_query($mysqli, $sql_sua);
+    if (mysqli_query($mysqli, $sql_xoa)) {
         header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
-//         exit; 
-//     } else {
-//         echo "Có lỗi khi cập nhật trạng thái!";
-//     }
-// } else {
-//     echo "ID không hợp lệ!";
-//}
+        exit;
+    } else {
+        echo "Có lỗi khi xóa dữ liệu!";
+    }
+} else {
+    echo "ID không hợp lệ!";
+}
 ?>
