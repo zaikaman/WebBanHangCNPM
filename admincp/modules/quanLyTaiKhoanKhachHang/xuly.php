6e4547584b06323527183a6ff5e5629617a5e749
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-08 11:24:27 +0700

    Update xuly.php

--- admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
@@ -6,13 +6,13 @@ $id = $_GET['id'];
 if (isset($id) && is_numeric($id)) {
     $sql_sua = "UPDATE tbl_taikhoan SET trang_thai = 2 WHERE id_dangky = '$id'";
 
-    if (mysqli_query($mysqli, $sql_sua)) {
+    // if (mysqli_query($mysqli, $sql_sua)) {
         header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
-        exit; 
-    } else {
-        echo "Có lỗi khi cập nhật trạng thái!";
-    }
-} else {
-    echo "ID không hợp lệ!";
+//         exit; 
+//     } else {
+//         echo "Có lỗi khi cập nhật trạng thái!";
+//     }
+// } else {
+//     echo "ID không hợp lệ!";
 }
 ?>
