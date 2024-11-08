cae58fcbc4c8419cf4590b92e79d029ec467307a
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-11-08 11:59:39 +0700

    Update xuly.php

--- admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
+++ admincp/modules/quanLyTaiKhoanKhachHang/xuly.php
@@ -2,7 +2,7 @@
 include('../../config/config.php');
 
 $id = $_GET['id'];
-    $sql_xoa = "DELETE FROM tbl_dangky WHERE id_dangky = '$id'";
+    $sql_xoa = "DELETE FROM tbl_dangky WHERE id='" . $id . "'";
     mysqli_query($mysqli, $sql_xoa);
     header('Location:../../index.php?action=quanLyTaiKhoanKhachHang&query=lietke');
         
