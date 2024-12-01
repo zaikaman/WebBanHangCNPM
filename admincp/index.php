75fc573fd7df3022d9c7409522ce0f9c9e63c779
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-12-01 11:01:44 +0700

    Update index.php

--- admincp/index.php
+++ admincp/index.php
@@ -38,8 +38,8 @@ if (!isset($_SESSION['dangNhap'])) {
         </nav>
 
     <div class="container-fluid mt-3 pt-3 px-3">
-            <div class="row">
-                <div class="col-md-3">
+            <div class="column">
+                <div class="row-md-3">
                     <?php include("modules/menu.php"); ?>
                 </div>
                 <div class="col-md-9">
