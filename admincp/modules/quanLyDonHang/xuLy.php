39728f07a6cc261738d2850eaf7f6ef3440af6df
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-12-01 10:52:29 +0700

    Update xuLy.php

--- admincp/modules/quanLyDonHang/xuLy.php
+++ admincp/modules/quanLyDonHang/xuLy.php
@@ -8,9 +8,8 @@ if(isset($_GET['code'])) {
     switch($action) {
         case 'process':
             // Update order status to processed
-            $sql = "UPDATE tbl_hoadon SET trang_thai = 0 WHERE ma_gh = ?";
-            $stmt = mysqli_prepare($mysqli, $sql);
-            mysqli_stmt_bind_param($stmt, $code_cart);
+            $sql = "UPDATE tbl_hoadon SET trang_thai = 0 WHERE ma_gh = '$code_cart'";
+            $query = mysqli_query($mysqli, $sql);
             
             if(mysqli_stmt_execute($stmt)) {
                 echo "<script>
