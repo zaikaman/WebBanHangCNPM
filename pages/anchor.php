58b9655379aff0aa24084076c65f63b628ef2f46
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-12-11 20:14:59 +0700

    Update anchor.php

--- pages/anchor.php
+++ pages/anchor.php
@@ -1,9 +1,4 @@
-// <div class="anchor">
-//     <a href="#header">
-//         <img style="  width: 27px;
-//         height: 30px;" src="../images/anchor.svg" alt="">
-//     </a>
-// </div>
+
 
 
 
